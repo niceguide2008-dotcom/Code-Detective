697a3e8eca8b6d763a143aba6697135137fd64ba
niceguide2008-dotcom <niceguide2008@gmail.com>
2026-08-04 20:08:38 +0530

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const fallbackUrl = 'https://mbtwdhadyonlirainmxm.supabase.co'
 const fallbackPublishableKey = 'sb_publishable_nf6tesnkk71a_NWhwFIjGA_VNtKJgCL'
