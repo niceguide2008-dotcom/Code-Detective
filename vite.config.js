8df88705f35867d4b0ac68092b7d29f3bb914fac
niceguide2008-dotcom <niceguide2008@gmail.com>
2026-08-06 00:50:06 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
+  plugins: [{
+    name: 'copy-investigation-pack-data',
+    closeBundle() {
+      copyFileSync(resolve(__dirname, 'java_oop_unit1.js'), resolve(__dirname, 'dist/java_oop_unit1.js'))
+    }
+  }],
   build: {
     rollupOptions: {
       input: {
